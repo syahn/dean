2a8a75bb22445882c88103b0001f78d5430ab49c
sangyoung.an <sangyoung.an@worksmobile.com>
2018-01-01 23:01:15 +0900

    Setup for development: babel, storybook

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import components from './components'
+
+const library = createLibrary(components)
+const Dean = Object.assign({}, library)
+
+export const {
+  Button
+} = library
+
+export default Dean
